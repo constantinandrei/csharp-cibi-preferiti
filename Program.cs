036b7903ec2ac349757df76d7ec51953a7df8069
Constantin Andrei Todirascu <andrei.todirascu@gmail.com>
2022-10-27 16:06:19 +0200

    snack 3 complete

--- Program.cs
+++ Program.cs
@@ -112,3 +112,21 @@ if (stringa1.Length > stringa2.Length)
 
 Console.WriteLine("--------------------------");
 Console.WriteLine();
+
+// Snack3. Il software deve chiedere per 10 volte all’utente di inserire un numero.
+//         Il programma stampa la somma di tutti i numeri inseriti
+
+Console.WriteLine("Snack 3");
+Console.WriteLine();
+
+int somma = 0;
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"Inserisci un numero ({i + 1} / 10)");
+    somma += Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("La somma dei numeri inseriti è: " + somma);
+
+Console.WriteLine("--------------------------");
+Console.WriteLine();
