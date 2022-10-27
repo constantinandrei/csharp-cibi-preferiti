a7cad98b51571c65e5b5e403f6d147a71dbd959b
Constantin Andrei Todirascu <andrei.todirascu@gmail.com>
2022-10-27 11:37:29 +0200

    adds comments

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 string[] cibiPreferiti = { "Pasta", "Pizza", "Hamburger", "Lenticchie", "Zucca", "Formaggio", "Zucchine", "Arance", "Riso", "Pomodori" };
 
+
+// La lunghezza della classifica
+
 Console.WriteLine("Lunghezza classifica: " + cibiPreferiti.Length);
 
 Console.WriteLine("--------------------------");
 Console.WriteLine();
 
+// La vostra classifica (dunque stampare l’intero array in ordine indicando la posizione in classifica)
 
 Console.WriteLine("Classifica:");
 
@@ -18,21 +22,32 @@ for (int i = 0; i < cibiPreferiti.Length; i++)
 Console.WriteLine("--------------------------");
 Console.WriteLine();
 
+// Il vostro cibo top (prima posizione della classifica)
+
 Console.WriteLine("Primo piatto in classifica:");
 Console.WriteLine(cibiPreferiti[0]);
 
 Console.WriteLine("--------------------------");
 Console.WriteLine();
 
+// Il vostro cibo preferito ma non troppo (ultima posizione della classifica)
+
 Console.WriteLine("Ultimo piatto in classifica (ma sempre preferito):");
 Console.WriteLine(cibiPreferiti[cibiPreferiti.Length - 1]);
 
 Console.WriteLine("--------------------------");
 Console.WriteLine();
+
+// BONUS Stampate a video anche il cibo di mezza classifica, cioè che si trova nella posizione mediana.
+// Attenzione: gestire anche il caso se aveste una classifica con un numero di elementi pari.
+// In questo caso vanno stampati i 2 elementi in centro alla vostra classifica.
+
 Console.WriteLine("Bonus: ");
 
+// stampa il valore mediano
 int posizioneMediana = cibiPreferiti.Length / 2;
 
+// se l'array ha lunghezza pari stampiamo anche la posizione precedente
 if (cibiPreferiti.Length % 2 == 0)
 {
     Console.WriteLine(cibiPreferiti[posizioneMediana -1 ]);
